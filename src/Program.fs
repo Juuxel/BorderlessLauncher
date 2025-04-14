99243daaab4feef1e0c0310dd7cdffde503b739f
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-04-14 21:10:34 +0300

    Rename and document --black-bars -> --letterboxing

--- src/Program.fs
+++ src/Program.fs
@@ -11,7 +11,7 @@ type Arguments =
     | [<Unique; AltCommandLine("-t")>] Timeout of int
     | [<AltCommandLine("-a")>] Keep_Aspect_Ratio
     | [<AltCommandLine("-d")>] Dodge_Taskbar
-    | [<AltCommandLine("-b")>] Black_Bars
+    | [<AltCommandLine("-b")>] Letterboxing
 
     interface IArgParserTemplate with
         member s.Usage =
@@ -20,7 +20,7 @@ type Arguments =
             | Timeout _ -> "the extra time to wait after the main window has been created (in milliseconds)"
             | Keep_Aspect_Ratio -> "retain the aspect ratio of the original window"
             | Dodge_Taskbar -> "if the process cannot be fullscreened, also avoids placing it on top of the taskbar (in conjunction with -a)"
-            | Black_Bars -> "if the process cannot be fullscreened, create a black bar window behind it (in conjunction with -a)"
+            | Letterboxing -> "if the process cannot be fullscreened, create a black bar window behind it (in conjunction with -a)"
 
 [<EntryPoint>]
 let main argv =
@@ -30,6 +30,6 @@ let main argv =
     let timeout = parseResults.TryGetResult Timeout
     let keepAspectRatio = parseResults.Contains Keep_Aspect_Ratio
     let dodgeTaskbar = parseResults.Contains Dodge_Taskbar
-    let blackBars = parseResults.Contains Black_Bars
+    let blackBars = parseResults.Contains Letterboxing
     Launcher.launch (List.head args) (List.tail args) timeout keepAspectRatio dodgeTaskbar blackBars
     0
