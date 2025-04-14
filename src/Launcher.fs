7c78964b79229741f1ddbc5c9468994673454722
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-04-14 15:28:10 +0300

    Initial commit

---
+++ src/Launcher.fs
@@ -0,0 +1,143 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+module BorderlessLauncher.Launcher
+
+open System.Diagnostics
+open System.Runtime.InteropServices
+
+type Size =
+    { Width: int
+      Height: int }
+    member self.CrossMultiply other =
+        self.Width * other.Height - self.Height * other.Width
+
+[<Struct>]
+[<StructLayout(LayoutKind.Sequential)>]
+type Rect =
+    { mutable Left: int
+      mutable Top: int
+      mutable Right: int
+      mutable Bottom: int }
+    static member Zero() = { Left = 0; Top = 0; Right = 0; Bottom = 0 }
+    member self.Width = self.Right - self.Left
+    member self.Height = self.Bottom - self.Top
+    member self.Size = { Width = self.Width; Height = self.Height }
+
+module private Native =
+    [<System.Flags>]
+    type SwpFlags =
+        | SWP_FRAMECHANGED = 0x0020u
+        | SWP_NOOWNERZORDER = 0x0200u
+        | SWP_NOZORDER = 0x0004u
+
+    let SWP_DEFAULT = SwpFlags.SWP_FRAMECHANGED ||| SwpFlags.SWP_NOOWNERZORDER ||| SwpFlags.SWP_NOZORDER
+
+    [<System.Flags>]
+    type WsFlags =
+        | WS_VISIBLE = 0x10000000L
+        | WS_CLIPCHILDREN = 0x02000000L
+
+    let WS_DEFAULT = WsFlags.WS_VISIBLE ||| WsFlags.WS_CLIPCHILDREN
+
+    // Native types
+    type HWND = nativeint
+    type HMONITOR = nativeint
+    type DWORD = uint
+
+    [<Struct>]
+    [<StructLayout(LayoutKind.Sequential)>]
+    type MonitorInfo =
+        { mutable CbSize: DWORD
+          mutable Monitor: Rect
+          mutable WorkArea: Rect
+          mutable Flags: DWORD }
+        static member Default() =
+            { CbSize = uint sizeof<MonitorInfo>
+              Monitor = Rect.Zero()
+              WorkArea = Rect.Zero()
+              Flags = 0u }
+
+    // Native constants
+    let HWND_TOPMOST: HWND = -1
+    let MONITOR_DEFAULTTOPRIMARY: DWORD = 1u
+    let GWL_STYLE = -16
+
+    [<DllImport("user32.dll")>]
+    extern HMONITOR MonitorFromWindow(HWND hWnd, DWORD dwFlags)
+
+    [<DllImport("user32.dll")>]
+    extern bool GetMonitorInfoW(HMONITOR hMonitor, [<Out>] MonitorInfo& lpmi)
+
+    [<DllImport("user32.dll")>]
+    extern bool GetWindowRect(HWND hWnd, [<Out>] Rect& rect)
+
+    [<DllImport("user32.dll")>]
+    extern bool SetWindowPos(HWND hWnd, HWND hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags)
+
+    [<DllImport("user32.dll")>]
+    extern int64 SetWindowLongPtrW(HWND hWnd, int nIndex, int64 dwNewLong)
+
+let private getWindowRect handle =
+    let mutable rect = Rect.Zero()
+    let success = Native.GetWindowRect(handle, &rect)
+#if DEBUG
+    printfn "Window rect %A, success %b" rect success
+#endif
+    rect
+
+let private getMonitorInfo window =
+    let monitor = Native.MonitorFromWindow(window, Native.MONITOR_DEFAULTTOPRIMARY)
+    let mutable monitorInfo = Native.MonitorInfo.Default()
+    let success = Native.GetMonitorInfoW(monitor, &monitorInfo)
+#if DEBUG
+    printfn "Monitor %d, monitor info %A, success %b" monitor monitorInfo success
+#endif
+    monitorInfo
+
+let launch (processName: string) (args: string list) (timeout: int option) (keepAspectRatio: bool) (dodgeTaskbar: bool) =
+    use proc = Process.Start(processName, args)
+    proc.WaitForInputIdle() |> ignore
+    if timeout.IsSome then
+        System.Threading.Thread.Sleep timeout.Value
+
+    let handle = proc.MainWindowHandle
+    let monitorInfo = getMonitorInfo handle
+    let monitorRect = monitorInfo.Monitor
+    let monitorSize = monitorRect.Size
+    let targetSize, viewportSize =
+        if keepAspectRatio then
+            let windowSize = getWindowRect handle |> _.Size
+            let c = windowSize.CrossMultiply monitorSize
+            if c = 0 then
+                // Same aspect ratio
+                monitorSize, monitorSize
+            else
+                let c, viewportSize =
+                    if dodgeTaskbar then
+                        let workAreaSize = monitorInfo.WorkArea.Size
+                        windowSize.CrossMultiply workAreaSize, workAreaSize
+                    else
+                        c, monitorSize
+                if c = 0 then
+                    viewportSize, viewportSize
+                else if c > 0 then
+                    // Window is wider
+                    let width = viewportSize.Width
+                    let height = float windowSize.Height / float windowSize.Width * float width |> round |> int
+                    { Width = width; Height = height }, viewportSize
+                else
+                    // Window is narrower
+                    let height = viewportSize.Height
+                    let width = float windowSize.Width / float windowSize.Height * float height |> round |> int
+                    { Width = width; Height = height }, viewportSize
+        else
+            monitorSize, monitorSize
+    let targetX = monitorRect.Left + (viewportSize.Width - targetSize.Width) / 2
+    let targetY = monitorRect.Top + (viewportSize.Height - targetSize.Height) / 2
+#if DEBUG
+    printfn "Target pos (%d, %d), size: %A" targetX targetY targetSize
+#endif
+    Native.SetWindowPos(handle, Native.HWND_TOPMOST, targetX, targetY, targetSize.Width, targetSize.Height, uint Native.SWP_DEFAULT) |> ignore
+    Native.SetWindowLongPtrW(handle, Native.GWL_STYLE, int64 Native.WS_DEFAULT) |> ignore
---
+++ src/Program.fs
@@ -0,0 +1,32 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+module BorderlessLauncher.Main
+
+open Argu
+
+type Arguments =
+    | [<MainCommand; ExactlyOnce; Last>] Process of args: string list
+    | [<Unique; AltCommandLine("-t")>] Timeout of int
+    | [<AltCommandLine("-a")>] Keep_Aspect_Ratio
+    | [<AltCommandLine("-d")>] Dodge_Taskbar
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Process _ -> "the process and its arguments"
+            | Timeout _ -> "the extra time to wait after the main window has been created (in milliseconds)"
+            | Keep_Aspect_Ratio -> "retain the aspect ratio of the original window"
+            | Dodge_Taskbar -> "if the process cannot be fullscreened, also avoids placing it on top of the taskbar (in conjunction with -a)"
+
+[<EntryPoint>]
+let main argv =
+    let parser = ArgumentParser.Create<Arguments>(programName = "borderlesslauncher.exe", errorHandler = ProcessExiter())
+    let parseResults = parser.ParseCommandLine argv
+    let args = parseResults.GetResult Process
+    let timeout = parseResults.TryGetResult Timeout
+    let keepAspectRatio = parseResults.Contains Keep_Aspect_Ratio
+    let dodgeTaskbar = parseResults.Contains Dodge_Taskbar
+    Launcher.launch (List.head args) (List.tail args) timeout keepAspectRatio dodgeTaskbar
+    0
